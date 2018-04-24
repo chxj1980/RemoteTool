3ca6cb28ae92cc8d756334a6f407dd19ee51b7a0
lcmftianci <707010543@qq.com>
2018-04-24 00:47:24 -0700

    add netutil && add screenutil

---
+++ EveNetShare/simple/clientdir/SimpleClient.cpp
@@ -0,0 +1,131 @@
+#include<sys/types.h>  
+#include<sys/socket.h>  
+#include<netinet/in.h>  
+#include<arpa/inet.h>  
+#include<errno.h>  
+#include<unistd.h>  
+#include<iostream>    
+#include<stdio.h>  
+#include<string.h>  
+#include<stdlib.h>  
+#include<event.h>  
+#include<event2/util.h>  
+      
+int tcp_connect_server(const char* server_ip, int port);        
+void cmd_msg_cb(int fd, short events, void* arg);  
+void socket_read_cb(int fd, short events, void *arg);  
+      
+int main(int argc, char** argv)  
+{  
+    if( argc < 3 )  
+    {  
+        printf("please input 2 parameter\n");  
+        return -1;  
+    }  
+        
+    //两个参数依次是服务器端的IP地址、端口号  
+	//1
+	int sockfd = tcp_connect_server(argv[1], atoi(argv[2]));  
+    if( sockfd == -1)  
+    {  
+        perror("tcp_connect error ");  
+        return -1;  
+	}  
+	printf("connect to server successful\n");  
+
+	//5
+    struct event_base* base = event_base_new();  
+    //6
+	struct event *ev_sockfd = event_new(base, sockfd, EV_READ | EV_PERSIST, socket_read_cb, NULL);  
+    //7
+	event_add(ev_sockfd, NULL);  
+      
+    //监听终端输入事件  
+	//8
+	struct event* ev_cmd = event_new(base, STDIN_FILENO, EV_READ | EV_PERSIST, cmd_msg_cb, (void*)&sockfd);  
+	
+	//struct event* ev_cmd = event_new(base, 0, EV_READ|EV_PERSIST, cmd_msg_cb, (void*)&sockfd);
+
+	//9
+	event_add(ev_cmd, NULL);  
+    //10
+	event_base_dispatch(base);  
+    printf("finished \n");  
+	return 0;  
+}  
+            
+void cmd_msg_cb(int fd, short events, void* arg)  
+{  
+	char msg[1024];  
+    //11
+//	int ret = 100;
+    int ret = read(fd, msg, sizeof(msg));  
+	//sprintf(msg, "fuck server");
+	//printf("%s\n", msg);
+	if( ret <= 0 )  
+    {  
+		perror("read fail ");  
+        exit(1);  
+    }  
+      
+	int sockfd = *((int*)arg);  
+      
+    //把终端的消息发送给服务器端  
+	//为了简单起见，不考虑写一半数据的情况  
+    //12
+	write(sockfd, msg, ret);  
+}  
+      
+//
+void socket_read_cb(int fd, short events, void *arg)  
+{  
+	char msg[1024];  
+      
+    //为了简单起见，不考虑读一半数据的情况  
+	//13
+	//int len = 100;
+	int len = read(fd, msg, sizeof(msg)-1);
+	std::cout << "hello server" << std::endl;
+    if( len <= 0 )  
+	{  
+        perror("read fail ");  
+		exit(1);  
+    }  
+      
+    msg[len] = '\0';    
+	printf("recv %s from server\n", msg);  
+}  
+      
+typedef struct sockaddr SA;  
+int tcp_connect_server(const char* server_ip, int port)  
+{  
+	int sockfd, status, save_errno;  
+    struct sockaddr_in server_addr;  
+      
+	memset(&server_addr, 0, sizeof(server_addr) );  
+	server_addr.sin_family = AF_INET;  
+    server_addr.sin_port = htons(port);  
+	status = inet_aton(server_ip, &server_addr.sin_addr);  
+    if( status == 0 ) //the server_ip is not valid value  
+	{  
+        errno = EINVAL;  
+		return -1;  
+    }  
+    //2  
+    sockfd = ::socket(PF_INET, SOCK_STREAM, 0);  
+	if( sockfd == -1 )  
+        return sockfd;  
+    //3        
+    status = ::connect(sockfd, (SA*)&server_addr, sizeof(server_addr) );  
+      
+    if( status == -1 )  
+    {  
+		save_errno = errno;  
+        ::close(sockfd);  
+        errno = save_errno; //the close may be error  
+		return -1;  
+    }  
+    //4  
+    evutil_make_socket_nonblocking(sockfd);        
+    return sockfd;  
+}  
---
+++ screencut/main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: lcmf
+	> Mail: mrhlingchen@163.com 
+	> Created Time: Mon 23 Apr 2018 11:08:41 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	printf("abg\n");
+	return 0;
+}
